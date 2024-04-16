4d09bdb2e915ca0748ef71580639d82bd48054da
Yasiru Hasantha <92869313+NotYasiru@users.noreply.github.com>
2024-04-16 20:15:53 +0530

    Add files via upload

---
+++ distraction_slayer_arduino/distraction_slayer_arduino.ino
@@ -0,0 +1,16 @@
+int x; 
+void setup() { 
+	Serial.begin(9600); 
+	Serial.setTimeout(1); 
+  pinMode(7, OUTPUT);
+} 
+void loop() { 
+	while (!Serial.available()); 
+	x = Serial.readString().toInt();
+  if(x == 2){
+    digitalWrite(7, HIGH);
+  }
+  else if(x == 1){
+    digitalWrite(7, LOW);
+  }
+}
